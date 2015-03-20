a3e94d6e194d42844f1db0db67a9ef8e550dfe4a
Luan Roger Santos Santana <luanengcomputacao@gmail.com>
2015-03-20 20:14:00 -0300

    Classe Carrinho de Compras com mais responsabilidades, entretanto, com tarefas contextuais internalizadas

--- CarrinhoDeCompras/src/tests/MaiorPrecoTest.java
+++ CarrinhoDeCompras/src/tests/MaiorPrecoTest.java
@@ -15,32 +15,27 @@ public class MaiorPrecoTest {
     @Test
     public void deveRetornarZeroSeCarrinhoVazio() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-        MaiorPreco algoritmo = new MaiorPreco();
-        double valor = algoritmo.encontra(carrinho);
 
-        assertEquals(0.0, valor, 0.0001);
+        assertEquals(0.0, carrinho.maiorValor(), 0.0001);
     }
 
     @Test
     public void deveRetornarValorDoItemSeCarrinhoComUmElemento() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-        MaiorPreco algoritmo = new MaiorPreco();
-        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-        double valor = algoritmo.encontra(carrinho);
 
-        assertEquals(900.0, valor, 0.0001);
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+
+        assertEquals(900.0, carrinho.maiorValor(), 0.0001);
     }
 
     @Test
     public void deveRetornarMaiorValorSeCarrinhoContemVariosElementos() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-        MaiorPreco algoritmo = new MaiorPreco();
+
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         carrinho.adiciona(new Item("Fogão", 1, 1500));
         carrinho.adiciona(new Item("Maquina de Lavar", 1, 750));
 
-        double valor = algoritmo.encontra(carrinho);
-
-        assertEquals(900.0, valor, 0.0001);
+        assertEquals(900.0, carrinho.maiorValor(), 0.0001);
     }
 }
