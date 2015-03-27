4e664724b557a9ef0fd1a56ff4e48ed697a700ee
Luan Roger Santos Santana <luanengcomputacao@gmail.com>
2015-03-27 00:36:55 -0300

    put before code to tests

--- CarrinhoDeCompras/src/tests/MaiorPrecoTest.java
+++ CarrinhoDeCompras/src/tests/MaiorPrecoTest.java
@@ -3,6 +3,7 @@ package tests;
 import classes.CarrinhoDeCompras;
 import classes.Item;
 import classes.MaiorPreco;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,17 +13,22 @@ import static org.junit.Assert.*;
  */
 public class MaiorPrecoTest {
 
+
+    private CarrinhoDeCompras carrinho;
+
+    @Before
+    public void inicializa() {
+        this.carrinho = new CarrinhoDeCompras();
+    }
+
+
     @Test
     public void deveRetornarZeroSeCarrinhoVazio() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-
         assertEquals(0.0, carrinho.maiorValor(), 0.0001);
     }
 
     @Test
     public void deveRetornarValorDoItemSeCarrinhoComUmElemento() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
 
         assertEquals(900.0, carrinho.maiorValor(), 0.0001);
@@ -30,8 +36,6 @@ public class MaiorPrecoTest {
 
     @Test
     public void deveRetornarMaiorValorSeCarrinhoContemVariosElementos() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         carrinho.adiciona(new Item("Fogão", 1, 1500));
         carrinho.adiciona(new Item("Maquina de Lavar", 1, 750));
